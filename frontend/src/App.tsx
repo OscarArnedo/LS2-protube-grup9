89c1438b6cd8b48dec7a48a4c4c3f998160c1175
Alba Lahora <122970333+alahoraT@users.noreply.github.com>
2024-10-22 12:51:21 +0200

    Create login DTO

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,37 @@
 import './App.css';
 import Form from './components/Form';
+import { useState } from 'react';
+import type { UserDTO } from './types/userDTO';
 
+//import { BrowserRouter, Routes,Route,Outlet,Link } from 'react-router-dom';
 
+interface AppState {
+  users: Array<UserDTO>
+  newUser: number
+}
+
+const INITIAL_STATE =[
+  {id: 1, username: 'admin', password: 'admin'},
+  {id: 2, username: 'user', password: 'user'}
+]
 
 function App() {
-  return (
-    <div className="App">
+  const [user, setUser] = useState<Array<UserDTO>>(INITIAL_STATE);
+  return(
+    <div>
       <h1>Login</h1>
       <Form />
     </div>
   );
+
+  /*return (<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Form />} />
+      </Routes>
+  </BrowserRouter>
+  );*/
+  
 }
+
 
 export default App;
--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,14 +1,33 @@
+import {useState } from 'react';
+import { UserDTO } from '../types/userDTO';
+
+
+interface FormState {
+    inputValues: UserDTO
+}
+
+//Empezar el FORMSPROPS
 const Form =() =>{
+    const [inputValues, setInputValues] = useState<FormState["inputValues"]>({ 
+        username: '',
+        password: ''
+    })
+
     const handleSubmit = () => {}
-    
+
+    const handleChange = (evt:React.ChangeEvent<HTMLInputElement>)  => { 
+        setInputValues({
+            ...inputValues,
+            [evt.target.name]: evt.target.value
+        })
+    }
+
     return(
         <div>   
             <form onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input type="text" />
-                <label>Password</label>
-                <input type="password" />
-                <button type="submit">Submit</button>
+                <input onChange={handleChange} value={inputValues.username} type="text" name='username' placeholder='Name'/>
+                <input onChange={handleChange} value={inputValues.password} type="password" name='password' placeholder='password'/>
+                <button>Submit</button>
             </form>
         </div>
     )
