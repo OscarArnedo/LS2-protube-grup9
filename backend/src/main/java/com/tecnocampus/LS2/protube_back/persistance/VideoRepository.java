504ab0f23c726fd5075cb068c807d4a1bd7fa6a7
LuisAcostaSicilia <lacostas@edu.tecnocampus.cat>
2024-11-20 18:57:29 +0100

    Get videos by Author and Test done

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/persistance/VideoRepository.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/persistance/VideoRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
     List<Video> getVideosByOwner(User owner);
+
+    List<Video> getVideosByOwnerId(String authorId);
 }
--- backend/src/main/java/com/tecnocampus/LS2/protube_back/service/VideoService.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/service/VideoService.java
@@ -68,4 +68,10 @@ public class VideoService {
 
         return videoMetaDataDTO;
     }
+
+    public List<VideoDTO> getVideosByAuthor(String name) throws Exception {
+        User author = userRepository.findByName(name).orElseThrow(() -> new EntityNotFound(User.class, "name", name));
+        List<Video> videos = videoRepository.getVideosByOwnerId(author.getId());
+        return videos.stream().map(VideoDTO::new).collect(Collectors.toList());
+    }
 }
